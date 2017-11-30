81198d7f2d8820e8ce33bc38a32cf553b0bedcba
HuyBTran <HuyBTran@5CD7074KR8.slcusd.k12.ca.us>
2017-11-30 09:02:43 -0800

    Solve for X supported. Kinda perfect now :)

--- EquationSolver/src/equationsolver/EquationSolver.java
+++ EquationSolver/src/equationsolver/EquationSolver.java
@@ -15,8 +15,9 @@ public class EquationSolver extends JApplet
         implements ActionListener {
 
         private JTextField display;
-        private JLabel guide1Label = new JLabel("Use only 2 numbers please!"); //instructions added
-        private JLabel guide2Label = new JLabel("operators: +-*/%^"); // instructions added
+        private JLabel guide1Label = new JLabel("Use only 2 numbers please!"); //instruction added
+        private JLabel guide2Label = new JLabel("operators: +-*/%^"); // instruction added
+        private JLabel guide3Label = new JLabel("Solve for X supported"); //instruction added
         private static final String ARROW = "==> ";
 
         @Override
@@ -32,6 +33,7 @@ public class EquationSolver extends JApplet
                 c.add(display);
                 c.add(guide1Label);
                 c.add(guide2Label);
+                c.add(guide3Label);
                 display.selectAll();
                 display.requestFocus();
         }
@@ -47,7 +49,7 @@ public class EquationSolver extends JApplet
                 display.selectAll();
         }
         
-        String t1, op, t2;
+        String t1, op, t2, t3;
         //t1 is the first number
         //op is the operator
         //t2 is the second number
@@ -55,11 +57,24 @@ public class EquationSolver extends JApplet
                 t1 = "";
                 t2 = "";
                 op = "";
-                separate(s); //below 
+                t3 = "";
+                if (s.indexOf("=") != -1){ //if there is a "=", it is a equation to solve for x
+                        int a = s.indexOf("="); // a is where the "=" in String s
+                        t3 = s.substring(a+1); // t3 is the number after "=" sign
+                        s = s.substring(0,a); // to make "separate(s)" work, s should be just "x + 2" instead of "x + 2 = 3";
+                }
+                separate(s); //below: divide String s into t1,t2,op
                 if (isNumber(t1) && isNumber(t2)) { //below: check if t1 and t2 are numbers
                         return evaluate(t1, op, t2);
-                } else {
-                        return ARROW + "Syntax Error";
+                }
+                if (isNumber(t3)){ //if t3 is a number, then it is a equation to solve for x 
+                        if (t1.contains("x")) 
+                                return solveforx(t3,op,t2); //if t1 is x (ex: x + 2 = 3) then put in (3,+,2) 
+                        else 
+                                return solveforx(t3,op,t1);// if t2 is x (ex: 2 + x = 3) then put in (3,+,2)
+                }
+                else {
+                        return ARROW + "Syntax error";
                 }
         }
 
@@ -72,23 +87,47 @@ public class EquationSolver extends JApplet
                 }
                 return result; //Return true if it is a number
         }
-
+        
         private void separate(String str) { //get t1, op, t2
                 int n = str.length(); 
                 String sign = "+-*/%^";  //A string contains all the operators that can be used
                 for (int i = 0; i < n; i++) {
                         if (sign.indexOf(str.substring(i, i + 1)) != -1) { // If str at index i is a operator supported
                                 t1 = str.substring(0, i);     // then I seperate the string and get the values :)
-                                op = str.substring(i, i + 1); 
+                                op = str.substring(i, i + 1);
                                 t2 = str.substring(i + 1);
                                 break;
                         }
-                }
+                }              
         }
 
+        private String solveforx(String term1, String op, String term2){ //solve for x with term1 is the number after the = sign;
+                String result; 
+                char opSign = op.charAt(0);
+                double a = Double.parseDouble(term1);
+                double b = Double.parseDouble(term2);
+                switch (opSign){
+                        case '+':
+                                result = ";x = " + (a - b);
+                                break;
+                        case '-':
+                                result = ";x = " + (a + b);
+                                break;
+                        case '*':
+                                result = ";x = " + (a / b);
+                                break;
+                        case '/':
+                                result = ";x = " + (a * b);
+                                break;
+                        default:
+                                result = ARROW + "Not supported";
+                                break;
+                }
+                return result;
+        }
         private String evaluate(String term1, String op, String term2) {
                 String result;
-               
+       
                 char opSign = op.charAt(0);
                 double a = Double.parseDouble(term1);
                 double b = Double.parseDouble(term2);
@@ -97,7 +136,7 @@ public class EquationSolver extends JApplet
                         case '+':
                                 result = "= " + (a + b);
                                 break;
-
+                                
                         case '-':
                                 result = "= " + (a - b);
                                 break;
