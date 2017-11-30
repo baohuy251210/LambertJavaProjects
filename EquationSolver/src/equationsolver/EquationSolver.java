ec498450b6facfb0d353fbf06e6d2b11ef56397a
cyot49 <baohuy251210@gmail.com>
2017-11-29 16:30:42 -0800

    EquationSolver. UPD(11/29/17 4.26PM)

--- EquationSolver/src/equationsolver/EquationSolver.java
+++ EquationSolver/src/equationsolver/EquationSolver.java
@@ -16,28 +16,29 @@ public class EquationSolver extends JApplet
         implements ActionListener {
 
         private JTextField display;
+        private JLabel guide1Label = new JLabel("Use only 2 numbers please!"); //instructions added
+        private JLabel guide2Label = new JLabel("operators: +-*/%^"); // instructions added
         private static final String ARROW = "==> ";
 
+        @Override
         public void init() {
                 display = new JTextField(40);
                 display.setFont(new Font("Monospaced", Font.BOLD, 14));
                 display.setBackground(Color.white);
                 display.setForeground(Color.blue);
                 display.addActionListener(this);
-                display.setText("Equation Solver by s/o");
+                display.setText("0.0 + 0.0 = 0.0");
                 Container c = getContentPane();
                 c.setLayout(new FlowLayout());
                 c.add(display);
-
-// Select the whole display text:
+                c.add(guide1Label);
+                c.add(guide2Label);
                 display.selectAll();
-
-// Prepare for typing:
                 display.requestFocus();
         }
 
+        @Override
         public void actionPerformed(ActionEvent e) {
-// Skip if display has any selected text:
                 if (display.getSelectedText() != null) {
                         return;
                 }
@@ -46,49 +47,49 @@ public class EquationSolver extends JApplet
                 display.setText(text + " " + process(text));
                 display.selectAll();
         }
-
+        
+        String t1, op, t2;
+        //t1 is the first number
+        //op is the operator
+        //t2 is the second number
         private String process(String s) {
-                separate(s);
-                if (isNumber(t1) && isNumber(t2)){
-                        return evaluate(t1,op,t2);
+                t1 = "";
+                t2 = "";
+                op = "";
+                separate(s); //below 
+                if (isNumber(t1) && isNumber(t2)) { //below: check if t1 and t2 are numbers
+                        return evaluate(t1, op, t2);
+                } else {
+                        return ARROW + "Syntax Error";
                 }
-                else return ARROW + "Syntax Error" + t1;
         }
 
-        private boolean isNumber(String str) {
+        private boolean isNumber(String str) { //Check if a string str is a number
                 boolean result = true;
                 try {
-                        Double.parseDouble(str);
-                } catch (Exception e) {
-                        result = false;
+                        Double.parseDouble(str);  //Try to turn the string to a number
+                } catch (Exception e) { //If cannot transform it 
+                        result = false; //then it is not a number :)
                 }
-                return result;
+                return result; //Return true if it is a number
         }
-        //
-        String t1, op, t2;
 
-        private void separate(String str) {
-                int n = str.length();
+        private void separate(String str) { //get t1, op, t2
+                int n = str.length(); 
+                String sign = "+-*/%^";  //A string contains all the operators that can be used
                 for (int i = 0; i < n; i++) {
-                        if (!Character.isDigit(str.charAt(i)) && !(str.substring(i, i + 1)).equals(".")) {
-                                t1 = str.substring(0, i);
-                                op = str.substring(i, i + 1);
+                        if (sign.indexOf(str.substring(i, i + 1)) != -1) { // If str at index i is a operator supported
+                                t1 = str.substring(0, i);     // then I seperate the string and get the values :)
+                                op = str.substring(i, i + 1); 
                                 t2 = str.substring(i + 1);
+                                break;
                         }
                 }
         }
 
-        /**
-         * Evaluates an arithmetic expression in the form "a +/- b" and returns
-         * the result in the form "= c" or an error message.
-         */
         private String evaluate(String term1, String op, String term2) {
                 String result;
-
-                if (op.length() != 1) {
-                        return ARROW + "Syntax error";
-                }
-
+               
                 char opSign = op.charAt(0);
                 double a = Double.parseDouble(term1);
                 double b = Double.parseDouble(term2);
@@ -111,8 +112,18 @@ public class EquationSolver extends JApplet
                                         result = "= " + (a / b);
                                 }
                                 break;
+                        case '%':
+                                if (b == 0) {
+                                        result = ARROW + "Invalid modulo";
+                                } else {
+                                        result = "= " + (a % b);
+                                }
+                                break;
+                        case '^':
+                                result = "= " + Math.pow(a, b);
+                                break;
                         default:
-                                result = ARROW + opSign + "is Invalid operation";
+                                result = ARROW + "Syntax error";
                                 break;
                 }
                 return result;
