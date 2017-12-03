d7fa8c128d2d89e47d64743663c64f1343add357
cyot49 <baohuy251210@gmail.com>
2017-12-03 02:31:51 -0800

    Check this out :)

---
+++ EquationSolver_1/src/equationsolver/EquationS.java
@@ -0,0 +1,16 @@
+package equationsolver;
+
+/**
+ *
+ * @author HuyBTran
+ */
+public class EquationS {
+
+        /**
+         * @param args the command line arguments
+         */
+        public static void main(String[] args) {
+                // TODO code application logic here
+        }
+        
+}
